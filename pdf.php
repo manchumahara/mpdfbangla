d35c65680912823887785f8b1726df4515c2c7f2
Sabuj Kundu <manchumahara@gmail.com>
2021-12-14 00:30:38 +0600

    invoice style added from mpdf example

--- pdf.php
+++ pdf.php
@@ -13,7 +13,38 @@ $html = '
         <title>MPDF</title>
 
         <!-- file css for style on report  -->
-        <link rel="stylesheet" href="./index.css">
+        <link rel="stylesheet" href="./index.css" />
+        <style>
+        p {	margin: 0pt; }
+table.items {
+	border: 0.1mm solid #000000;
+}
+td { vertical-align: top; }
+.items td {
+	border-left: 0.1mm solid #000000;
+	border-right: 0.1mm solid #000000;
+}
+table thead td { background-color: #EEEEEE;
+	text-align: center;
+	border: 0.1mm solid #000000;
+	font-variant: small-caps;
+}
+.items td.blanktotal {
+	background-color: #EEEEEE;
+	border: 0.1mm solid #000000;
+	background-color: #FFFFFF;
+	border: 0mm none #000000;
+	border-top: 0.1mm solid #000000;
+	border-right: 0.1mm solid #000000;
+}
+.items td.totals {
+	text-align: right;
+	border: 0.1mm solid #000000;
+}
+.items td.cost {
+	text-align: "." center;
+}
+</style>
     </head>
       <body>
         <div class="container">
@@ -25,148 +56,220 @@ $html = '
                         <div class="brand_company">
                             For testing report MPDF in বাংলা ভাষা
                         </div>
-                        <div class="description">
-                            <small>Testing purpose</small>
-                        </div>
+                       
                     </div>
                 </div>
             </div>
-            <!-- body of report  -->
-            <div class="row">
-                <div class="body_report">
-                    <div class="name_header">
-                        <p>Name                 : Testing name</p>
-                        <p>Age                  : 12 </p>
-                        <p>Date of birth        : 10/11/1999</p>
-                        <p>Test Date            : 10/11/1999 </p>
-                        <p>School               : RUPP </p>
-                        <p>Grade                : Y1 </p>
-                    </div>
-                    <div class="Header_title">
-                        <strong>
-                         Header of this
-                                   </strong>
-                    </div>
-                    <div class="container_details">
-                        <!-- it should be table of report  -->
-                        <table>
-                            <thead>
-                                <tr>
-                                    <td>
-                                     Name
-                                    </td>
-                                    <td>
-                                     Age
-                                    </td>
-                                    <td>
-                                     Date of Birth
-                                             </td>
-                                    <td>
-                                     Test Date
-                                          </td>
-                                    <td>
-                                     School
-                                    </td>
-                                    <td>
-                                     Grade
-                                    </td>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                     testing1
-                                    </td>
-                                    <td>
-                                     10
-                                    </td>
-                                    <td>
-                                     10/11/1999
-                                    </td>
-                                    <td>
-                                     10/11/1999
-                                    </td>
-                                    <td>
-                                     RUPP
-                                    </td>
-                                    <td>
-                                     Year1
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                     testing1
-                                    </td>
-                                    <td>
-                                     10
-                                    </td>
-                                    <td>
-                                     10/11/1999
-                                    </td>
-                                    <td>
-                                     10/11/1999
-                                    </td>
-                                    <td>
-                                     RUPP
-                                    </td>
-                                    <td>
-                                     Year1
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                     testing1
-                                    </td>
-                                    <td>
-                                     10
-                                    </td>
-                                    <td>
-                                     10/11/1999
-                                    </td>
-                                    <td>
-                                     10/11/1999
-                                    </td>
-                                    <td>
-                                     RUPP
-                                    </td>
-                                    <td>
-                                     Year1
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                     testing1
-                                    </td>
-                                    <td>
-                                     10
-                                    </td>
-                                    <td>
-                                     10/11/1999
-                                    </td>
-                                    <td>
-                                     10/11/1999
-                                    </td>
-                                    <td>
-                                     RUPP
-                                    </td>
-                                    <td>
-                                     Year1
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <!-- footer of report  -->
-            <div class="row">
-                <div class="address">
-                           Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, animi!
-                </div>
-                <div class="tel">
-                           012121212212
-                </div>
-            </div>
+            <!--mpdf
+<htmlpageheader name="myheader">
+<table width="100%"><tr>
+<td width="50%" style="color:#0000BB; "><span style="font-weight: bold; font-size: 14pt;">Acme Trading Co.</span><br />123 Anystreet<br />Your City<br />GD12 4LP<br /><span style="font-family:dejavusanscondensed;">&#9742;</span> 01777 123 567</td>
+<td width="50%" style="text-align: right;">Invoice No.<br /><span style="font-weight: bold; font-size: 12pt;">0012345</span></td>
+</tr></table>
+</htmlpageheader>
+<htmlpagefooter name="myfooter">
+<div style="border-top: 1px solid #000000; font-size: 9pt; text-align: center; padding-top: 3mm; ">
+Page {PAGENO} of {nb}
+</div>
+</htmlpagefooter>
+<sethtmlpageheader name="myheader" value="on" show-this-page="1" />
+<sethtmlpagefooter name="myfooter" value="on" />
+mpdf-->
+<div style="text-align: right">Date: 13th November 2008</div>
+<table width="100%" style="font-family: serif;" cellpadding="10"><tr>
+<td width="45%" style="border: 0.1mm solid #888888; "><span style="font-size: 7pt; color: #555555; font-family: sans;">SOLD TO:</span><br /><br />345 Anotherstreet<br />Little Village<br />Their City<br />CB22 6SO</td>
+<td width="10%">&nbsp;</td>
+<td width="45%" style="border: 0.1mm solid #888888;"><span style="font-size: 7pt; color: #555555; font-family: sans;">SHIP TO:</span><br /><br />345 Anotherstreet<br />Little Village<br />Their City<br />CB22 6SO</td>
+</tr></table>
+<br />
+<table class="items" width="100%" style="font-size: 9pt; border-collapse: collapse; " cellpadding="8">
+<thead>
+<tr>
+<td width="15%">Ref. No.</td>
+<td width="10%">Quantity</td>
+<td width="45%">Description</td>
+<td width="15%">Unit Price</td>
+<td width="15%">Amount</td>
+</tr>
+</thead>
+<tbody>
+<!-- ITEMS HERE -->
+<tr>
+<td align="center">MF1234567</td>
+<td align="center">10</td>
+<td>Large pack Hoover bags</td>
+<td class="cost">&pound;2.56</td>
+<td class="cost">&pound;25.60</td>
+</tr>
+<tr>
+<td align="center">MX37801982</td>
+<td align="center">1</td>
+<td>Womans waterproof jacket<br />Options - Red and charcoal.</td>
+<td class="cost">&pound;102.11</td>
+<td class="cost">&pound;102.11</td>
+</tr>
+<tr>
+<td align="center">MR7009298</td>
+<td align="center">25</td>
+<td>Steel nails; oval head; 30mm x 3mm. Packs of 1000.</td>
+<td class="cost">&pound;12.26</td>
+<td class="cost">&pound;325.60</td>
+</tr>
+<tr>
+<td align="center">MF1234567</td>
+<td align="center">10</td>
+<td>Large pack Hoover bags</td>
+<td class="cost">&pound;2.56</td>
+<td class="cost">&pound;25.60</td>
+</tr>
+<tr>
+<td align="center">MX37801982</td>
+<td align="center">1</td>
+<td>Womans waterproof jacket<br />Options - Red and charcoal.</td>
+<td class="cost">&pound;102.11</td>
+<td class="cost">&pound;102.11</td>
+</tr>
+<tr>
+<td align="center">MR7009298</td>
+<td align="center">25</td>
+<td>Steel nails; oval head; 30mm x 3mm. Packs of 1000.</td>
+<td class="cost">&pound;12.26</td>
+<td class="cost">&pound;325.60</td>
+</tr>
+<tr>
+<td align="center">MF1234567</td>
+<td align="center">10</td>
+<td>Large pack Hoover bags</td>
+<td class="cost">&pound;2.56</td>
+<td class="cost">&pound;25.60</td>
+</tr>
+<tr>
+<td align="center">MX37801982</td>
+<td align="center">1</td>
+<td>Womans waterproof jacket<br />Options - Red and charcoal.</td>
+<td class="cost">&pound;102.11</td>
+<td class="cost">&pound;102.11</td>
+</tr>
+<tr>
+<td align="center">MR7009298</td>
+<td align="center">25</td>
+<td>Steel nails; oval head; 30mm x 3mm. Packs of 1000.</td>
+<td class="cost">&pound;12.26</td>
+<td class="cost">&pound;325.60</td>
+</tr>
+<tr>
+<td align="center">MF1234567</td>
+<td align="center">10</td>
+<td>Large pack Hoover bags</td>
+<td class="cost">&pound;2.56</td>
+<td class="cost">&pound;25.60</td>
+</tr>
+<tr>
+<td align="center">MX37801982</td>
+<td align="center">1</td>
+<td>Womans waterproof jacket<br />Options - Red and charcoal.</td>
+<td class="cost">&pound;102.11</td>
+<td class="cost">&pound;102.11</td>
+</tr>
+<tr>
+<td align="center">MR7009298</td>
+<td align="center">25</td>
+<td>Steel nails; oval head; 30mm x 3mm. Packs of 1000.</td>
+<td class="cost">&pound;12.26</td>
+<td class="cost">&pound;325.60</td>
+</tr>
+<tr>
+<td align="center">MF1234567</td>
+<td align="center">10</td>
+<td>Large pack Hoover bags</td>
+<td class="cost">&pound;2.56</td>
+<td class="cost">&pound;25.60</td>
+</tr>
+<tr>
+<td align="center">MX37801982</td>
+<td align="center">1</td>
+<td>Womans waterproof jacket<br />Options - Red and charcoal.</td>
+<td class="cost">&pound;102.11</td>
+<td class="cost">&pound;102.11</td>
+</tr>
+<tr>
+<td align="center">MF1234567</td>
+<td align="center">10</td>
+<td>Large pack Hoover bags</td>
+<td class="cost">&pound;2.56</td>
+<td class="cost">&pound;25.60</td>
+</tr>
+<tr>
+<td align="center">MX37801982</td>
+<td align="center">1</td>
+<td>Womans waterproof jacket<br />Options - Red and charcoal.</td>
+<td class="cost">&pound;102.11</td>
+<td class="cost">&pound;102.11</td>
+</tr>
+<tr>
+<td align="center">MR7009298</td>
+<td align="center">25</td>
+<td>Steel nails; oval head; 30mm x 3mm. Packs of 1000.</td>
+<td class="cost">&pound;12.26</td>
+<td class="cost">&pound;325.60</td>
+</tr>
+<tr>
+<td align="center">MR7009298</td>
+<td align="center">25</td>
+<td>Steel nails; oval head; 30mm x 3mm. Packs of 1000.</td>
+<td class="cost">&pound;12.26</td>
+<td class="cost">&pound;325.60</td>
+</tr>
+<tr>
+<td align="center">MF1234567</td>
+<td align="center">10</td>
+<td>Large pack Hoover bags</td>
+<td class="cost">&pound;2.56</td>
+<td class="cost">&pound;25.60</td>
+</tr>
+<tr>
+<td align="center">MX37801982</td>
+<td align="center">1</td>
+<td>Womans waterproof jacket<br />Options - Red and charcoal.</td>
+<td class="cost">&pound;102.11</td>
+<td class="cost">&pound;102.11</td>
+</tr>
+<tr>
+<td align="center">MR7009298</td>
+<td align="center">25</td>
+<td>Steel nails; oval head; 30mm x 3mm. Packs of 1000.</td>
+<td class="cost">&pound;12.26</td>
+<td class="cost">&pound;325.60</td>
+</tr>
+<!-- END ITEMS HERE -->
+<tr>
+<td class="blanktotal" colspan="3" rowspan="6"></td>
+<td class="totals">Subtotal:</td>
+<td class="totals cost">&pound;1825.60</td>
+</tr>
+<tr>
+<td class="totals">Tax:</td>
+<td class="totals cost">&pound;18.25</td>
+</tr>
+<tr>
+<td class="totals">Shipping:</td>
+<td class="totals cost">&pound;42.56</td>
+</tr>
+<tr>
+<td class="totals"><b>TOTAL:</b></td>
+<td class="totals cost"><b>&pound;1882.56</b></td>
+</tr>
+<tr>
+<td class="totals">Deposit:</td>
+<td class="totals cost">&pound;100.00</td>
+</tr>
+<tr>
+<td class="totals"><b>Balance due:</b></td>
+<td class="totals cost"><b>&pound;1782.56</b></td>
+</tr>
+</tbody>
+</table>
+<div style="text-align: center; font-style: italic;">Payment terms: payment due in 30 days</div>                      
         </div>
     </body>
     </html>';
@@ -187,8 +290,18 @@ $mpdf = new \Mpdf\Mpdf( [
 		],
 ] );
 
-$mpdf->SetHeader('Document Common Header Title|Center Text|{PAGENO}');
-$mpdf->SetFooter('Document common footer Title');
+//same header and footer can be set from html too
+//$mpdf->SetHeader('Document Common Header Title|Center Text|{PAGENO}');
+//$mpdf->SetFooter('Document common footer Title');
+
+$mpdf->SetProtection(array('print'));
+$mpdf->SetTitle("Acme Trading Co. - Invoice");
+$mpdf->SetAuthor("Acme Trading Co.");
+$mpdf->SetWatermarkText("Paid");
+$mpdf->showWatermarkText = true;
+$mpdf->watermark_font = 'DejaVuSansCondensed';
+$mpdf->watermarkTextAlpha = 0.1;
+$mpdf->SetDisplayMode('fullpage');
 
 $mpdf->WriteHTML( $html );
 $mpdf->Output();
